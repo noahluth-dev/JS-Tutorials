a957d8cc94dface41a95f429c3fa4972e61218dd
Sinna_Shan <luth595@gmail.com>
2025-10-06 19:25:18 +0530

    values and variables

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let js = 'java script හොදයි.'
-console.log(js);
+// ===== Values & Variable =====
+
+// 1. Number එකකිං පටං ගන්න බෑ.
+// 2. Space තියෙන්න බෑ.
+// 3. $,_ හැර වෙන spatial characters වලින් පටන්ගන්න බෑ.
+// 4. Keywords පාවිච්චි කරන්න බෑ.
+
+// ** Descriptive variable names පාවිච්චි කරන්න.
+// ** (snake_case or camelCase) පාවිච්චි කරන්න.
+
+let firstName = 'Noah';
+console.log(firstName);
